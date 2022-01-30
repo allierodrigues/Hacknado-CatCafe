7d2c8982b3379f41693d654bf2f67fd1426a6610
0allie <82842213+0allie@users.noreply.github.com>
2022-01-30 08:22:21 -0600

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,9 +1,5 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 class Cat
